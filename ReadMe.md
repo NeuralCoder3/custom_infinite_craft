a41bb037230ffcd6466968f92de3dd7805a55c54
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-03-07 07:49:41 +0100

    image generation

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,21 @@
+# SDXL+LLAMA Craft
 
-# Features
+
+
+## Features
 
 - [x] Create high-quality words based on state-of-the-art LLMs (e.g. Mixtral)
-- [ ] Create fancy icons based on Stable-Diffusion
+- [x] Create fancy icons based on Stable-Diffusion
 - [ ] A website to play with the combinations
     - [ ] a tree/graph to keep track of your combinations
+
+
+## Explanation
+
+## APIs
+
+## Related
+
+- Infinite Craft: https://neal.fun/infinite-craft/
+- GPT Combination Guesser: https://github.com/YuraSuper2048/infinite-craft-gpt-server
+- Infinite Craft Related Repos: https://github.com/search?q=infinite+craft&type=repositories (mostly bruteforcer or enumerators)
--- combine.py
+++ combine.py
@@ -1,21 +1,47 @@
 from llama_cpp import Llama
+from diffusers import AutoPipelineForText2Image
+import torch
 import os
 import sys
 import shelve
+from PIL import Image
 
 path = "."
 model = "mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf"
-system_prompt = "You combine words according to their meaning. Avoid having the result be the same as one of the inputs. Try to generate general words. Be creative. Keep the words simple. Above all, always answer with a singular word or phrase delimited by quotes."
+system_prompt = """
+You combine words according to their meaning. 
+Avoid having the result be the same as one of the inputs. 
+Try to generate general words. 
+Be creative. 
+Keep the words simple. 
+Above all, always answer with a singular word or phrase delimited by quotes.
+"""
+system_prompt = system_prompt.replace("\n"," ").replace("  "," ").strip()
+combinations_file = "combinations"
 
+image_model = "stabilityai/sdxl-turbo"
+image_modifier = "concept art, design, icon"
+image_negative_prompt = "lowres, cropped, worst quality"
+# ordered in priority
+endings = ["png", "jpg"]
+image_folder = "images"
+image_enabled = True
 
 model_path = os.path.join(path,model)
 if not os.path.exists(model_path):
     print(f"Model file not found at {model_path}. Please download the model file first.")
     sys.exit(1)
 
+print("Loading models...")
+print("Loading llama...")
 llm = Llama(model_path=model_path, chat_format="llama-2",verbose=False) 
+if image_enabled:
+    print("Loading SDXL...")
+    image_pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float32, variant="fp16")
+else:
+    image_pipe = None
 
-combinations = shelve.open("combinations")
+combinations = shelve.open(combinations_file)
 
 def sanitize(element):
     element = element.lower()
@@ -25,13 +51,34 @@ def sanitize(element):
     element = element.strip()
     return element
     
+def get_image(element, generate=True, debug=False):
+    filename = element.replace(" ","_")
+    for ending in endings:
+        path = os.path.join(image_folder,f"{filename}.{ending}")
+        if os.path.exists(path):
+            return path
+    if not generate or not image_enabled:
+        return None
+    prompt = f"{element}, {image_modifier}"
+    path = os.path.join(image_folder,f"{filename}.{endings[0]}")
+    if debug:
+        print(f"Generating image for {element}")
+    image = image_pipe(prompt=prompt, negative_prompt=image_negative_prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    image.save(path)
+    return path
 
-def combine(element1,element2):
+def combine(element1,element2, generate=True, image=True, debug=False):
     element1, element2 = sorted([sanitize(element) for element in [element1,element2]])
     tuple = f"[{element1}]_[{element2}]"
+    generate_image = generate and image
     if tuple in combinations:
-        return combinations[tuple]
+        path = get_image(combinations[tuple], generate=generate_image)
+        return combinations[tuple], path
+    if not generate:
+        return None, None
         
+    if debug:
+        print(f"Combining {element1} and {element2}")
     response = llm.create_chat_completion(
         messages = [
             {"role": "system", "content": system_prompt},
@@ -51,7 +98,19 @@ def combine(element1,element2):
     
     combinations[tuple] = combined
     combinations.sync()
-    return combined
+    
+    if debug:
+        print(f"Result: {combined}")
+    
+    path = get_image(combined, generate=generate_image, debug=debug)
+    return combined, path
+
+
+
+
+
+
+
 
 print("Some example combinations:")
 for element1, element2 in [
@@ -90,7 +149,7 @@ def combine_route_post():
     data = request.json
     element1 = data["element1"]
     element2 = data["element2"]
-    combined = combine(element1,element2)
+    combined = combine(element1,element2, generate=True, image=True, debug=True)
     return jsonify({"combined":combined})
 
 @app.route('/combine_get', methods=['GET'])
@@ -100,49 +159,15 @@ def combine_route_get():
     # generally use POST instead
     element1 = request.args.get('element1')
     element2 = request.args.get('element2')
-    combined = combine(element1,element2)
+    generate = request.args.get('generate')
+    if generate is None:
+        generate = False
+    combined = combine(element1,element2, generate=generate)
+    if combined is None:
+        return jsonify({"error":"combination not found"})
     return jsonify({"combined":combined})
 
 # for testing (debug mode)
 app.run(port=PORT)
     
 combinations.close()
-
-
-
-
-
-
-
-# for element1, element2 in [
-#     ("fire", "water"),
-#     ("fire", "steam"),
-#     ("steam", "steam"),
-#     ("water", "water"),
-#     ("water", "steam"),
-#     ("steam", "water"),
-#     ("harry potter", "goblin"),
-#     ("lord of the rings", "goblin"),
-# ]:
-#     response = llm.create_chat_completion(
-#         messages = [
-#             {"role": "system", "content": "You combine words according to their meaning. Avoid having the result be the same as one of the inputs. Keep the words simple. Above all, always answer with a singular word or phrase delimited by quotes."},
-#             {
-#                 "role": "user",
-#                 "content": f"\"{element1}\" + \"{element2}\" = ?"
-#             },
-#         ],
-#         max_tokens=32,
-#         stop=["</s>", "\n"],
-#     )
-    
-#     combined = response["choices"][0]["message"]["content"].strip()
-#     # remove things in parentheses
-#     # combined = combined.split("(")[0].strip()
-#     # extract from quotes
-#     combined = combined.split('"')[1]
-
-#     print(f"{element1} + {element2} = {combined}")
-    
-
-# {'id': 'chatcmpl-4d06e928-db59-4df4-ada3-4aae84daac17', 'object': 'chat.completion', 'created': 1709748419, 'model': './mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': " Once upon a time, in the high Andes mountains of Peru, there was a group of wild llamas who lived freely and happily in the vast grasslands. These llamas were known for their beautiful, thick wool coats that came in a variety of colors - from creamy white to rich chocolate brown, and even speckled grey. They were also admired for their long, powerful legs and necks, which allowed them to reach the tastiest leaves and shrubs on the tallest hillsides.\n\nThe leader of the herd was a wise and gentle llama named Lucho. He was a magnificent creature, with a thick, shimmering coat of gold and a regal bearing that commanded respect from all who met him. Lucho was not only strong and brave, but he was also kind-hearted and always looked out for the well-being of his herd.\n\nOne day, while the llamas were grazing in the fields, they noticed a group of humans approaching from afar. The llamas had seen humans before, but these ones seemed different - they wore strange clothes and carried strange objects that the llamas had never seen before. Lucho knew that he had to protect his herd from any potential danger, so he called out to them in their unique, melodic language, warning them to stay away.\n\nBut the humans did not understand the llama's language, and they continued to approach. Lucho realized that he needed to find a way to communicate with these strange creatures. He decided to use his most valuable asset - his wool. Lucho carefully selected some of the finest strands from his golden coat and wove them into a beautiful, intricate pattern.\n\nWhen the humans saw the stunning creation, they were amazed. They had never seen anything like it before. The llamas' unique language had finally been understood, and the ice was broken. The humans and llamas began to communicate, and Lucho learned that these humans were not a threat at all. In fact, they had come to offer the llamas a gift - a new home where they would be protected from predators and cared for by humans who appreciated their beauty and grace.\n\nLucho was hesitant at first, but he knew that this was"}, 'finish_reason': 'length'}], 'usage': {'prompt_tokens': 32, 'completion_tokens': 480, 'total_tokens': 512}}
